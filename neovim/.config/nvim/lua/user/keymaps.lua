ae6556cb91567b946c3902d559d3c3e8299ff015
Neftales Antunes <neftales@gmail.com>
2022-01-09 20:29:38 -0300

    add plugin for fs tree.

--- neovim/.config/nvim/lua/user/keymaps.lua
+++ neovim/.config/nvim/lua/user/keymaps.lua
@@ -21,7 +21,6 @@ vim.g.maplocalleader = " "
 -- Normal --
 -- Better window navigation
 
-keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
@@ -62,3 +61,5 @@ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
 keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opts)
 keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
+
+keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
