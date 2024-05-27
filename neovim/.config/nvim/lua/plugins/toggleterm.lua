832741d722dcf6115c1d43977f57f1bcc8fbf4f9
Neftales Antunes <neftales@gmail.com>
2024-05-27 13:25:06 -0300

    Configura integração com terminal no lazyvim.

---
+++ neovim/.config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,24 @@
+return {
+  "akinsho/toggleterm.nvim",
+  keys = {
+    { [[<C-\>]] },
+  },
+  cmd = { "ToggleTerm", "TermExec" },
+  opts = {
+    size = 10,
+    hide_numbers = true,
+    open_mapping = [[<C-\>]],
+    shade_filetypes = {},
+    shade_terminals = false,
+    shading_factor = 0.3,
+    start_in_insert = true,
+    persist_size = true,
+    direction = "horizontal",
+    winbar = {
+      enabled = false,
+      name_formatter = function(term)
+        return term.name
+      end,
+    },
+  },
+}
